4fc9e575a105cd515b11305c03dc90a8a66b4902
summerlikey <summerlikey@163.com>
2020-06-23 18:55:32 +0800

    单个参数传入

--- useEmbed/great_module.py
+++ useEmbed/great_module.py
@@ -3,3 +3,12 @@ def great_function(a):
 
 def test1(b):
     return b + 2
+
+def test2(a,b):
+    return a + b
+
+def test_string(str):
+    s = 'The value of str is ' + str
+    print(s)
+    a = 11
+    return a
